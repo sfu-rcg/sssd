572666cbf57aeeafbbf247cff14d9bd512ca18d7
Riley Shott <rshott@sfu.ca>
2014-11-27 12:01:23 -0800

    Added basic spec framework
    After doing a `bundle install` you should be able to run `rake validate` to
    check the syntax of everything, and `rake spec` to check manifests logic

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+puppetversion = ENV.key?('PUPPET_VERSION') ? "= #{ENV['PUPPET_VERSION']}" : ['>= 3.3']
+gem 'puppet', puppetversion
+gem 'puppetlabs_spec_helper', '>= 0.1.0'
+gem 'puppet-lint', '>= 0.3.2'
+gem 'facter', '>= 1.7.0'
+gem 'rspec-puppet', :github => 'rodjek/rspec-puppet', :ref => '6ac97993fa972a15851a73d55fe3d1c0a85172b5'
---
+++ spec/classes/init_spec.rb
@@ -0,0 +1,125 @@
+require 'spec_helper'
+describe 'sssd', :type => 'class' do
+
+  context 'with domains => unset' do
+    it { expect { should compile }.to raise_error(/Must pass domains/) }
+  end
+  
+  context 'with domains => string' do
+    let :params do
+      {
+        :domains => 'string',
+      }
+    end
+    it { expect { should compile }.to raise_error(/not an Array/) }
+  end
+  
+  context 'with backends => string' do
+    let :params do
+      {
+        :domains  => ['value'],
+        :backends => 'string'
+      }
+    end
+    it { expect { should compile }.to raise_error(/must be a hash/) }
+  end
+  
+  context 'with make_home_dir => string' do
+    let :params do
+      {
+        :domains       => ['value'],
+        :make_home_dir => 'string'
+        
+      }
+    end
+    it { expect { should compile }.to raise_error(/is not a boolean/) }
+  end
+  
+  context 'with filter_users => string' do
+    let :params do
+      {
+        :domains       => ['value'],
+        :filter_users  => 'string'
+        
+      }
+    end
+    it { expect { should compile }.to raise_error(/not an Array/) }
+  end
+  
+  context 'with filter_groups => string' do
+    let :params do
+      {
+        :domains       => ['value'],
+        :filter_groups => 'string'
+        
+      }
+    end
+    it { expect { should compile }.to raise_error(/not an Array/) }
+  end
+  
+  context 'on a RedHat system' do
+    let :facts do
+      {
+        :osfamily       => 'RedHat',
+        :concat_basedir => '/dne',
+      }
+    end
+    
+    context 'with domains => set' do
+      let :params do
+        {
+          :domains       => [ 'mydomain.local' ],
+        }
+      end
+      it do
+        should compile.with_all_deps
+        contain_package('sssd').with_ensure('installed')
+        should contain_concat('sssd_conf').with(
+          'path' => '/etc/sssd/sssd.conf',
+          'mode' => '0600',
+        )
+        should contain_concat__fragment('sssd_conf_header').with(
+          'target'  => 'sssd_conf',
+          #'content' => 'template(sssd/header_sssd.conf.erb)',
+          'order'   => '10',
+        )
+        should contain_exec('authconfig-sssd').with_command('/usr/sbin/authconfig --enablesssd --enablesssdauth --enablelocauthorize --update')
+        should contain_service('sssd').with(
+          'ensure' => 'running',
+          'enable' => 'true',
+        ).that_subscribes_to('Exec[authconfig-sssd]')
+        should contain_service('crond').that_subscribes_to('Service[sssd]')
+      end
+    end
+    
+    context 'with domains => set, make_home_dir => true' do
+      let :params do
+        {
+          :domains       => [ 'mydomain.local' ],
+          :make_home_dir => true,
+        }
+      end
+      it do
+        should compile.with_all_deps
+        should contain_class('sssd::homedir')
+        contain_package('sssd').with_ensure('installed')
+        should contain_concat('sssd_conf').with(
+        'path' => '/etc/sssd/sssd.conf',
+        'mode' => '0600',
+        )
+        should contain_concat__fragment('sssd_conf_header').with(
+        'target'  => 'sssd_conf',
+        #'content' => 'template(sssd/header_sssd.conf.erb)',
+        'order'   => '10',
+        )
+        should contain_exec('authconfig-sssd').with_command('/usr/sbin/authconfig --enablesssd --enablesssdauth --enablelocauthorize --update')
+        should contain_service('sssd').with(
+        'ensure' => 'running',
+        'enable' => 'true',
+        ).that_subscribes_to('Exec[authconfig-sssd]')
+        should contain_service('crond').that_subscribes_to('Service[sssd]')
+      end
+    end
+    
+  end
+end
