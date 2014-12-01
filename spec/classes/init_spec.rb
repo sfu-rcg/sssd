34648f5ece56bb626c7c8fe01567529ffd5b9e36
Riley Shott <rshott@sfu.ca>
2014-11-30 23:14:30 -0800

    Updated spec tests
    Modified init_spec.rb for the new parameters, and added two new files for the
    two define types.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,60 +1,22 @@
 require 'spec_helper'
 describe 'sssd', :type => 'class' do
-
-  context 'with domains => unset' do
-    it { expect { should compile }.to raise_error(/Must pass domains/) }
-  end
   
-  context 'with domains => string' do
-    let :params do
+  context 'on an unsupported system' do
+    let :facts do
       {
-        :domains => 'string',
+        :osfamily       => 'notsupported',
       }
     end
-    it { expect { should compile }.to raise_error(/not an Array/) }
-  end
-  
-  context 'with backends => string' do
-    let :params do
-      {
-        :domains  => ['value'],
-        :backends => 'string'
-      }
+    
+    context 'with domains => set' do
+      let :params do
+        {
+          :domains => ['mydomain.com'],
+        }
+      end
+      it { expect { should compile }.to raise_error(/Unsupported distribution/) }
     end
-    it { expect { should compile }.to raise_error(/must be a hash/) }
-  end
-  
-  context 'with make_home_dir => string' do
-    let :params do
-      {
-        :domains       => ['value'],
-        :make_home_dir => 'string'
-        
-      }
-    end
-    it { expect { should compile }.to raise_error(/is not a boolean/) }
-  end
-  
-  context 'with filter_users => string' do
-    let :params do
-      {
-        :domains       => ['value'],
-        :filter_users  => 'string'
-        
-      }
-    end
-    it { expect { should compile }.to raise_error(/not an Array/) }
-  end
-  
-  context 'with filter_groups => string' do
-    let :params do
-      {
-        :domains       => ['value'],
-        :filter_groups => 'string'
-        
-      }
-    end
-    it { expect { should compile }.to raise_error(/not an Array/) }
+    
   end
   
   context 'on a RedHat system' do
@@ -64,11 +26,75 @@ describe 'sssd', :type => 'class' do
         :concat_basedir => '/dne',
       }
     end
+
+    context 'with domains => unset' do
+      it { expect { should compile }.to raise_error(/Must pass domains/) }
+    end
     
-    context 'with domains => set' do
+    context 'with domains => string' do
       let :params do
         {
-          :domains       => [ 'mydomain.local' ],
+          :domains => 'string',
+        }
+      end
+      it { expect { should compile }.to raise_error(/not an Array/) }
+    end
+    
+    context 'with services => string' do
+      let :params do
+        {
+          :domains => 'string',
+        }
+      end
+      it { expect { should compile }.to raise_error(/not an Array/) }
+    end
+    
+    context 'with options => string' do
+      let :params do
+        {
+          :domains => ['value'],
+          :options => 'string'
+        }
+      end
+      it { expect { should compile }.to raise_error(/not a Hash/) }
+    end
+    
+    context 'with sections => string' do
+      let :params do
+        {
+          :domains  => ['value'],
+          :options  => {'key' => 'value'},
+          :sections => 'string',
+        }
+      end
+      it { expect { should compile }.to raise_error(/not a Hash/) }
+    end
+    
+    context 'with backends => string' do
+      let :params do
+        {
+          :domains  => ['value'],
+          :backends => 'string'
+        }
+      end
+      it { expect { should compile }.to raise_error(/not a Hash/) }
+    end
+    
+    context 'with make_home_dir => string' do
+      let :params do
+        {
+          :domains       => ['value'],
+          :make_home_dir => 'string'
+          
+        }
+      end
+      it { expect { should compile }.to raise_error(/is not a boolean/) }
+    end
+    
+    context 'with domains => set, sections => default' do
+      let :params do
+        {
+          :domains       => [ 'mydomain.com' ],
         }
       end
       it do
@@ -92,10 +118,10 @@ describe 'sssd', :type => 'class' do
       end
     end
     
-    context 'with domains => set, make_home_dir => true' do
+    context 'with domains => set, sections => default, make_home_dir => true' do
       let :params do
         {
-          :domains       => [ 'mydomain.local' ],
+          :domains       => [ 'mydomain.com' ],
           :make_home_dir => true,
         }
       end
---
+++ spec/defines/service_spec.rb
@@ -0,0 +1,102 @@
+require 'spec_helper'
+
+describe 'sssd::service', :type => :define do
+  context "with a options => unset" do
+    let :title do
+      'nss'
+    end
+    it { expect { should compile }.to raise_error(/Must pass options/) }
+  end
+  
+  context "with a name => not_matched, options => set" do
+    let :title do
+      'notamatch'
+    end
+    let :params do
+      {
+        :options => {'key' => 'value'},
+      }
+    end
+    it { expect { should compile }.to raise_error(/does not match a supported service/) }
+  end
+  
+  context "with a options => string" do
+    let :title do
+      'nss'
+    end
+    let :params do
+      {
+        :options => 'string',
+      }
+    end
+    it { expect { should compile }.to raise_error(/not a Hash/) }
+  end
+  
+  context "with a options => set, concat_order => boolean" do
+    let :title do
+      'nss'
+    end
+    let :params do
+      {
+        :options      => {'key' => 'value'},
+        :concat_order => true,
+      }
+    end
+      it { expect { should compile }.to raise_error(/not a string/) }
+  end
+  
+  context "on a RedHat system" do
+    let :pre_condition do
+      "class { 'sssd':
+        domains  => [ 'mydomain.com' ],
+        sections => {},
+      }"
+    end
+    let :facts do
+      {
+        :osfamily       => 'RedHat',
+        :concat_basedir => '/dne',
+      }
+    end
+    
+    context "with a options => set" do
+      let :title do
+        'nss'
+      end
+      let :params do
+        {
+          :options      => {'key' => 'value'},
+        }
+      end
+      it do
+        should compile.with_all_deps
+        should contain_concat__fragment('sssd_service_nss').with(
+        'target'  => 'sssd_conf',
+        #'content' => 'template(sssd/domain_sssd.conf.erb)',
+        'order'   => '20',
+        )
+      end
+    end
+    
+    context "with a options => set, concat_order => set" do
+      let :title do
+        'nss'
+      end
+      let :params do
+        {
+          :options         => {'key' => 'value'},
+          :concat_order    => '15'
+        }
+      end
+      it do
+        should compile.with_all_deps
+        should contain_concat__fragment('sssd_service_nss').with(
+        'target'  => 'sssd_conf',
+        #'content' => 'template(sssd/domain_sssd.conf.erb)',
+        'order'   => '15',
+        )
+      end
+    end
+
+  end
+end
