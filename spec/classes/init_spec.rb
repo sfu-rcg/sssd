912be49f38aae4201e58e485ab3b694dbff0dcac
Riley Shott <rshott@sfu.ca>
2014-12-01 00:50:15 -0800

    Updated spec tests
    Added appropriate spec test sections for Debian machines.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -26,7 +26,167 @@ describe 'sssd', :type => 'class' do
         :concat_basedir => '/dne',
       }
     end
-
+    
+    context 'with domains => unset' do
+      it { expect { should compile }.to raise_error(/Must pass domains/) }
+    end
+    
+    context 'with domains => string' do
+      let :params do
+        {
+          :domains => 'string',
+        }
+      end
+      it { expect { should compile }.to raise_error(/not an Array/) }
+    end
+    
+    context 'with services => string' do
+      let :params do
+        {
+          :domains => 'string',
+        }
+      end
+      it { expect { should compile }.to raise_error(/not an Array/) }
+    end
+    
+    context 'with options => string' do
+      let :params do
+        {
+          :domains => ['value'],
+          :options => 'string'
+        }
+      end
+      it { expect { should compile }.to raise_error(/not a Hash/) }
+    end
+    
+    context 'with sections => string' do
+      let :params do
+        {
+          :domains  => ['value'],
+          :options  => {'key' => 'value'},
+          :sections => 'string',
+        }
+      end
+      it { expect { should compile }.to raise_error(/not a Hash/) }
+    end
+    
+    context 'with backends => string' do
+      let :params do
+        {
+          :domains  => ['value'],
+          :backends => 'string'
+        }
+      end
+      it { expect { should compile }.to raise_error(/not a Hash/) }
+    end
+    
+    context 'with make_home_dir => string' do
+      let :params do
+        {
+          :domains       => ['value'],
+          :make_home_dir => 'string'
+          
+        }
+      end
+      it { expect { should compile }.to raise_error(/is not a boolean/) }
+    end
+    
+    context 'with domains => set, sections => default' do
+      let :params do
+        {
+          :domains       => [ 'mydomain.com' ],
+        }
+      end
+      it do
+        should compile.with_all_deps
+        should_not contain_class('sssd::homedir')
+        contain_package('sssd').with_ensure('installed')
+        should contain_concat('sssd_conf').with(
+        'path' => '/etc/sssd/sssd.conf',
+        'mode' => '0600',
+        )
+        should contain_concat__fragment('sssd_conf_header').with(
+        'target'  => 'sssd_conf',
+        #'content' => 'template(sssd/header_sssd.conf.erb)',
+        'order'   => '10',
+        )
+        should contain_exec('authconfig-sssd').with_command('/usr/sbin/authconfig --enablesssd --enablesssdauth --enablelocauthorize --update')
+        should contain_service('sssd').with(
+        'ensure' => 'running',
+        'enable' => 'true',
+        ).that_subscribes_to('Exec[authconfig-sssd]')
+        should contain_service('crond').that_subscribes_to('Service[sssd]')
+      end
+    end
+    
+    context 'with domains => set, sections => default, make_home_dir => true' do
+      let :params do
+        {
+          :domains       => [ 'mydomain.com' ],
+          :make_home_dir => true,
+        }
+      end
+      it do
+        should compile.with_all_deps
+        should contain_class('sssd::homedir')
+        contain_package('sssd').with_ensure('installed')
+        should contain_concat('sssd_conf').with(
+        'path' => '/etc/sssd/sssd.conf',
+        'mode' => '0600',
+        )
+        should contain_concat__fragment('sssd_conf_header').with(
+        'target'  => 'sssd_conf',
+        #'content' => 'template(sssd/header_sssd.conf.erb)',
+        'order'   => '10',
+        )
+        should contain_exec('authconfig-sssd').with_command('/usr/sbin/authconfig --enablesssd --enablesssdauth --enablelocauthorize --update')
+        should contain_service('sssd').with(
+        'ensure' => 'running',
+        'enable' => 'true',
+        ).that_subscribes_to('Exec[authconfig-sssd]')
+        should contain_service('crond').that_subscribes_to('Service[sssd]')
+      end
+    end
+    
+    context 'with domains => set, sections => default, manage_cron => false' do
+      let :params do
+        {
+          :domains       => [ 'mydomain.com' ],
+          :manage_cron   => false,
+        }
+      end
+      it do
+        should compile.with_all_deps
+        # Again, for now
+        should_not contain_class('sssd::homedir')
+        contain_package('sssd').with_ensure('installed')
+        should contain_concat('sssd_conf').with(
+        'path' => '/etc/sssd/sssd.conf',
+        'mode' => '0600',
+        )
+        should contain_concat__fragment('sssd_conf_header').with(
+        'target'  => 'sssd_conf',
+        #'content' => 'template(sssd/header_sssd.conf.erb)',
+        'order'   => '10',
+        )
+        should contain_exec('authconfig-sssd').with_command('/usr/sbin/authconfig --enablesssd --enablesssdauth --enablelocauthorize --update')
+        should contain_service('sssd').with(
+        'ensure' => 'running',
+        'enable' => 'true',
+        ).that_subscribes_to('Exec[authconfig-sssd]')
+        should_not contain_service('crond')
+      end
+    end
+  end
+  
+  context 'on a Debian system' do
+    let :facts do
+      {
+        :osfamily       => 'Debian',
+        :concat_basedir => '/dne',
+      }
+    end
+    
     context 'with domains => unset' do
       it { expect { should compile }.to raise_error(/Must pass domains/) }
     end
@@ -101,20 +261,20 @@ describe 'sssd', :type => 'class' do
         should compile.with_all_deps
         contain_package('sssd').with_ensure('installed')
         should contain_concat('sssd_conf').with(
-          'path' => '/etc/sssd/sssd.conf',
-          'mode' => '0600',
+        'path' => '/etc/sssd/sssd.conf',
+        'mode' => '0600',
         )
         should contain_concat__fragment('sssd_conf_header').with(
-          'target'  => 'sssd_conf',
-          #'content' => 'template(sssd/header_sssd.conf.erb)',
-          'order'   => '10',
+        'target'  => 'sssd_conf',
+        #'content' => 'template(sssd/header_sssd.conf.erb)',
+        'order'   => '10',
         )
-        should contain_exec('authconfig-sssd').with_command('/usr/sbin/authconfig --enablesssd --enablesssdauth --enablelocauthorize --update')
+        should contain_exec('authconfig-sssd').with_command('/bin/true')
         should contain_service('sssd').with(
-          'ensure' => 'running',
-          'enable' => 'true',
+        'ensure' => 'running',
+        'enable' => 'true',
         ).that_subscribes_to('Exec[authconfig-sssd]')
-        should contain_service('crond').that_subscribes_to('Service[sssd]')
+        should contain_service('cron').that_subscribes_to('Service[sssd]')
       end
     end
     
@@ -127,7 +287,8 @@ describe 'sssd', :type => 'class' do
       end
       it do
         should compile.with_all_deps
-        should contain_class('sssd::homedir')
+        # Again, for now
+        should_not contain_class('sssd::homedir')
         contain_package('sssd').with_ensure('installed')
         should contain_concat('sssd_conf').with(
         'path' => '/etc/sssd/sssd.conf',
@@ -138,12 +299,42 @@ describe 'sssd', :type => 'class' do
         #'content' => 'template(sssd/header_sssd.conf.erb)',
         'order'   => '10',
         )
-        should contain_exec('authconfig-sssd').with_command('/usr/sbin/authconfig --enablesssd --enablesssdauth --enablelocauthorize --update')
+        should contain_exec('authconfig-sssd').with_command('/bin/true')
         should contain_service('sssd').with(
         'ensure' => 'running',
         'enable' => 'true',
         ).that_subscribes_to('Exec[authconfig-sssd]')
-        should contain_service('crond').that_subscribes_to('Service[sssd]')
+        should contain_service('cron').that_subscribes_to('Service[sssd]')
+      end
+    end
+    
+    context 'with domains => set, sections => default, manage_cron => false' do
+      let :params do
+        {
+          :domains       => [ 'mydomain.com' ],
+          :manage_cron   => false,
+        }
+      end
+      it do
+        should compile.with_all_deps
+        # Again, for now
+        should_not contain_class('sssd::homedir')
+        contain_package('sssd').with_ensure('installed')
+        should contain_concat('sssd_conf').with(
+        'path' => '/etc/sssd/sssd.conf',
+        'mode' => '0600',
+        )
+        should contain_concat__fragment('sssd_conf_header').with(
+        'target'  => 'sssd_conf',
+        #'content' => 'template(sssd/header_sssd.conf.erb)',
+        'order'   => '10',
+        )
+        should contain_exec('authconfig-sssd').with_command('/bin/true')
+        should contain_service('sssd').with(
+        'ensure' => 'running',
+        'enable' => 'true',
+        ).that_subscribes_to('Exec[authconfig-sssd]')
+        should_not contain_service('cron')
       end
     end
     
